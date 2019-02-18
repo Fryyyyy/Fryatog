0d4c644b5731b77937a470ae5bd51c3d109edfde
Andrew Villarrubia <andrew@primefour.com>
2019-02-18 10:15:22 -0600

    Refactor dice functionality.
    Move validation to its own function. Create Dice struct.

--- funsies.go
+++ funsies.go
@@ -1,29 +1,68 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand"
 	"regexp"
 	"strconv"
 	"strings"
 	"time"
+
+	log "gopkg.in/inconshreveable/log15.v2"
 )
+
+type Dice struct {
+	number   int
+	sides    int
+	operator byte
+	operand  int
+}
+
+func (d Dice) calculate() string {
+	rand.Seed(time.Now().UnixNano())
+	if d.sides == 0 {
+		return fmt.Sprintf("1 %d-sided die: %d", d.number, rand.Intn(d.number-1)+1)
+	}
+	var result int
+	for i := 1; i <= d.number; i++ {
+		result += rand.Intn(d.sides-1) + 1
+	}
+
+	if d.operand != 0 {
+		if d.operator == '+' {
+			result += d.operand
+		}
+		if d.operator == '-' {
+			result -= d.operand
+		}
+		return fmt.Sprintf("%d %d-sided dice (%c%d): %d", d.number, d.sides, d.operator, d.operand, result)
+	}
+	return fmt.Sprintf("%d %d-sided dice: %d", d.number, d.sides, result)
+
+}
 
 func RollDice(input string) string {
 	diceRegex := regexp.MustCompile(`^(\d+)(?:d?(\d+)([+-]\d+)?)?`)
-	dice := diceRegex.FindStringSubmatch(input)
-	rand.Seed(time.Now().UnixNano())
-	var operator byte
-	var modifier int
-	var typeDice int
-	failure := "roll: Try something like 'roll 4', 'roll 3d8', 'roll 2d6+2'"
+	inputDice := diceRegex.FindStringSubmatch(input)
 
-    //input didn't pass the regex at all so dice is empty
+	dice, err := validateDice(input, inputDice)
+	if err != nil {
+		return err.Error()
+	}
+	log.Debug(fmt.Sprintf("%dd%d%c%d", dice.number, dice.sides, dice.operator, dice.operand))
+	return dice.calculate()
+}
+
+func validateDice(input string, dice []string) (Dice, error) {
+
+	failure := "roll: Try something like 'roll 4', 'roll 3d8', 'roll 2d6+2'"
+	//input didn't pass the regex at all so dice is empty
 	if len(dice) == 0 {
-		return failure
+		return Dice{}, errors.New(failure)
 	}
 
-    //cuts out weirdly formed things
+	//cuts out weirdly formed things
 	failCases := []bool{
 		dice[2] == "" && len(input) > len(dice[1]),
 		strings.Contains(input, "d") && dice[2] == "",
@@ -32,53 +71,40 @@ func RollDice(input string) string {
 
 	for _, failCase := range failCases {
 		if failCase {
-			return failure
+			return Dice{}, errors.New(failure)
 		}
 	}
 
-	numDice, _ := strconv.Atoi(dice[1])
+	var (
+		sidesDice int
+		operator  byte
+		operand   int
+	)
+	numDice, err := strconv.Atoi(dice[1])
+	if err != nil || numDice > 99 {
+		return Dice{}, errors.New("That's too many dice. Calm down.")
+	}
 
-    if (numDice > 99) {
-        return "That's too many dice. Calm down."
-    }
-
-	if dice[2] == "" {
-		return fmt.Sprintf("1 %d-sided die: %d", numDice, rand.Intn(numDice-1)+1)
+	if dice[2] != "" {
+		sidesDice, err = strconv.Atoi(dice[2])
+		if err != nil || sidesDice > 100 {
+			return Dice{}, errors.New("When have you ever seen a die with that many sides? Come on.")
+		}
+		if sidesDice <= 1 {
+			return Dice{}, errors.New("Your spherical dice go careening off the flat earth. You know. Those two things that exist.")
+		}
 	}
 
 	if dice[3] != "" {
 		operator = dice[3][0]
-		modifier, _ = strconv.Atoi(dice[3][1:])
-        if (modifier > 1000) {
-            return "Haha. No. Why do you need a mod that large?"
-        }
+		operand, err = strconv.Atoi(dice[3][1:])
+		if err != nil || operand > 1000 {
+			return Dice{}, errors.New("Haha. No. Why do you need a mod that large?")
+		}
 	}
 
-	if dice[2] != "" {
-		typeDice, _ = strconv.Atoi(dice[2])
-		if typeDice == 1 {
-			return "Your spherical dice go careening off the flat earth. You know. Those two things that exist."
-		}
-
-        if (typeDice > 100) {
-            return "When have you ever seen a die with that many sides? Come on."
-        }
-
-		var result int
-
-		for i := 1; i <= numDice; i++ {
-			result += (rand.Intn(typeDice-1) + 1)
-		}
-
-		if operator == '+' {
-			result += modifier
-		}
-		if operator == '-' {
-			result -= modifier
-		}
-		return fmt.Sprintf("%d %d-sided dice %s: %d", numDice, typeDice, dice[3], result)
-	}
-	return ""
+	wellFormedDice := Dice{numDice, sidesDice, operator, operand}
+	return wellFormedDice, nil
 }
 
 func FlipCoin(user string) string {
--- funsies_test.go
+++ funsies_test.go
@@ -1,72 +1,73 @@
 package main
 
 import (
-    "regexp"
-    "testing"
+	"regexp"
+	"testing"
 )
 
 func TestFlipCoin(t *testing.T) {
-    
-    for i := 1; i < 10; i++ {
-        result := FlipCoin("testUser")
-        validOutput := regexp.MustCompile(`testUser flips a coin: (?:heads|tails).`)
-        if !(validOutput.MatchString(result)) {
-            t.Errorf(`FAIL: Expected heads or tails in output,
+
+	for i := 1; i < 10; i++ {
+		result := FlipCoin("testUser")
+		validOutput := regexp.MustCompile(`testUser flips a coin: (?:heads|tails).`)
+		if !(validOutput.MatchString(result)) {
+			t.Errorf(`FAIL: Expected heads or tails in output,
                       but result was \"%s\"`, result)
-        }
-    }
+		}
+	}
 }
 
 func TestRollDice(t *testing.T) {
 
-    plannedFailure := "roll: Try something like 'roll 4', 'roll 3d8', 'roll 2d6+2'"
-    tables := []struct {
-        input string
-        expected string
-    } {
-        {"", plannedFailure},
-        {" ", plannedFailure},
-        {"kd4", plannedFailure},
-        {"2d6+_", plannedFailure},
-        {"2daf3", plannedFailure},
-        {"2d6+e", plannedFailure},
-        {"2e6?_", plannedFailure},
-        {"-3d8", plannedFailure},
-        {"3d1", "Your spherical dice go careening off the flat earth. You know. Those two things that exist."},
-        {"2d112", "When have you ever seen a die with that many sides? Come on."},
-        {"100d12", "That's too many dice. Calm down."},
-        {"4d8+1200", "Haha. No. Why do you need a mod that large?"},
-        {"92233720368547758072131231", "That's too many dice. Calm down."},
-        {"4d92233720368547758073213123", "When have you ever seen a die with that many sides? Come on."},
-    }
+	plannedFailure := "roll: Try something like 'roll 4', 'roll 3d8', 'roll 2d6+2'"
+	tables := []struct {
+		input    string
+		expected string
+	}{
+		{"", plannedFailure},
+		{" ", plannedFailure},
+		{"kd4", plannedFailure},
+		{"2d6+_", plannedFailure},
+		{"2daf3", plannedFailure},
+		{"2d6+e", plannedFailure},
+		{"2e6?_", plannedFailure},
+		{"-3d8", plannedFailure},
+		{"3d1", "Your spherical dice go careening off the flat earth. You know. Those two things that exist."},
+		{"2d112", "When have you ever seen a die with that many sides? Come on."},
+		{"4d0", "Your spherical dice go careening off the flat earth. You know. Those two things that exist."},
+		{"100d12", "That's too many dice. Calm down."},
+		{"4d8+1200", "Haha. No. Why do you need a mod that large?"},
+		{"92233720368547758072131231", "That's too many dice. Calm down."},
+		{"4d92233720368547758073213123", "When have you ever seen a die with that many sides? Come on."},
+	}
 
-    validateOutput := regexp.MustCompile(`\d+ \d+-sided dic?e(?::|\s(?:[+-]\d)?:) \d+`)
-    randTables := []struct {
-        input string
-    } {
+	validateOutput := regexp.MustCompile(`\d+ \d+-sided dic?e(?::|\s(?:\([+-]\d\))?:) \d+`)
+	randTables := []struct {
+		input string
+	}{
 
-        {"4"},
-        {"4d6"},
-        {"2d20+3"},
-        {"2d6-2"},
-        {"50d8"},
-    }
+		{"4"},
+		{"4d6"},
+		{"2d20+3"},
+		{"2d6-2"},
+		{"50d8"},
+	}
 
-    for _, table := range tables {
-        result := RollDice(table.input)
-        if !(result == table.expected) {
-            t.Errorf("FAIL: Input %s: Expected %s -- Got %s", table.input, table.expected, result)
-        } else {
-            t.Logf("OK: %s", table.input)
-        }
-    }
+	for _, table := range tables {
+		result := RollDice(table.input)
+		if !(result == table.expected) {
+			t.Errorf("FAIL: Input %s: Expected %s -- Got %s", table.input, table.expected, result)
+		} else {
+			t.Logf("OK: %s", table.input)
+		}
+	}
 
-    for _, table := range randTables {
-        result := RollDice(table.input)
-        if !(validateOutput.MatchString(result)) {
-            t.Errorf("FAIL: Input %s: Got %s", table.input, result)
-        } else {
-            t.Logf("OK: %s -> %s", table.input, result)
-        }
-    }
+	for _, table := range randTables {
+		result := RollDice(table.input)
+		if !(validateOutput.MatchString(result)) {
+			t.Errorf("FAIL: Input %s: Got %s", table.input, result)
+		} else {
+			t.Logf("OK: %s -> %s", table.input, result)
+		}
+	}
 }
