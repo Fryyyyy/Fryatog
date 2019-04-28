de5fc7bd57106b071705cc97c79aa313fcd77e3d
Andrew Villarrubia <andrew@primefour.com>
2019-04-28 03:02:13 -0500

    Add better documentation for Mairsil rulings case.

--- card.go
+++ card.go
@@ -727,7 +727,12 @@ func (card *Card) sortRulings() error {
 	// Scryfall doesn't acknowledge Gatherer ruling order as canonical,
 	// so their "ruling 11" is Gatherer's "ruling 1." Yes, I hate this.
 	if card.Name == "Mairsil, the Pretender" {
+		// Since append requires a slice as its first arg, so we can't just do card.Rulings[11]
+		// We can't use negative indices in Go like we can in Python, so we get a slice that's
+		// just the last object in the list.
 		card.Rulings = append(card.Rulings[len(card.Rulings)-1:], card.Rulings...)
+		// Get a slice that drops the last ruling, since we just moved it to the front of the list
+		// and we don't want it duplicated. 
 		card.Rulings = card.Rulings[:len(card.Rulings)-1]
 		return nil
 	}
