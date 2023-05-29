ebcb5c92db233bde36f34cf5ccdd4544a37dd040
Vaclav Lunak <v.lunak@gmail.com>
2023-05-29 11:28:05 +0200

    move constants to top of file

--- card.go
+++ card.go
@@ -27,6 +27,9 @@ const scryfallSearchAPIURL = "https://api.scryfall.com/cards/search"
 const highlanderPointsURL = "http://decklist.mtgpairings.info/js/cards/highlander.txt"
 
 const noFlavourText = "Flavour text not found"
+const fullyShownSearchThreshold = 2 // limit for how many search results are fully displayed (inclusive)
+const shownSearchThreshold = 20 // limit for how many search results are listed as names (inclusive)
+
 
 // TODO: Also CardFaces
 func (card *Card) getExtraMetadata(inputURL string) {
@@ -706,10 +709,7 @@ func searchScryfallCard(cardTokens []string) ([]Card, error) {
 }
 
 func ParseAndFormatSearchResults(csr CardSearchResult) ([]Card, error) {
-	fullyShownThreshold := 2 // limit for how many results are fully displayed (inclusive)
-	shownThreshold := 20 // limit for how many results are listed as names (inclusive)
-
-	if len(csr.Data) <= shownThreshold {
+	if len(csr.Data) <= shownSearchThreshold {
 		for _, c := range csr.Data {
 			x := c
 			cNcn := normaliseCardName(c.Name)
@@ -727,13 +727,13 @@ func ParseAndFormatSearchResults(csr CardSearchResult) ([]Card, error) {
 	switch {
 	case csr.TotalCards == 0:
 		return []Card{}, fmt.Errorf("No cards found")
-	case csr.TotalCards <= fullyShownThreshold:
-		minLen := min(fullyShownThreshold, len(csr.Data))
+	case csr.TotalCards <= fullyShownSearchThreshold:
+		minLen := min(fullyShownSearchThreshold, len(csr.Data))
 		return csr.Data[0:minLen], nil
-	case csr.TotalCards > shownThreshold:
-		return []Card{}, fmt.Errorf("Too many cards returned (%v > %v)", csr.TotalCards, shownThreshold)
+	case csr.TotalCards > shownSearchThreshold:
+		return []Card{}, fmt.Errorf("Too many cards returned (%v > %v)", csr.TotalCards, shownSearchThreshold)
 	default:
-		// Between fullyShownThreshold and shownThreshold number of cards
+		// Between fullyShownSearchThreshold and shownSearchThreshold number of cards
 		var names []string
 		for _, c := range csr.Data {
 			names = append(names, c.Name)
